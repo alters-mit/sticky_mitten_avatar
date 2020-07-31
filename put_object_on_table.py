f2769e19186a97fcb0ad072b8a49f94e934587d2
alters-mit <alters@mit.edu>
2020-07-31 12:54:47 -0400

    works

--- put_object_on_table.py
+++ put_object_on_table.py
@@ -1,13 +1,20 @@
 import numpy as np
 from distutils import dir_util
 from pathlib import Path
-from typing import Tuple, List, Optional, Union
+from typing import Tuple, List, Optional, Union, Dict
 from tdw.controller import Controller
 from tdw.tdw_utils import TDWUtils
 from tdw.output_data import OutputData, AvatarChildrenNames, AvatarStickyMitten, Images, Bounds
 
 
 class PutObjectOnTable(Controller):
+    """
+    1. Create a Sticky Mitten Avatar, a 3rd-person camera, an object, and a table.
+    2. The avatar picks up the object.
+    3. The avatar moves to the table.
+    4. The avatar puts the object on the table.
+    """
+
     OUTPUT_DIR = Path("images")
     # Remove the previous images.
     if OUTPUT_DIR.exists():
@@ -23,7 +30,7 @@ class PutObjectOnTable(Controller):
         # Camera ID.
         self.cam_id = "c"
 
-    def run(self):
+    def run(self) -> None:
         self.start()
 
         # Get IDs for the object, the table, and the avatars.
@@ -41,6 +48,7 @@ class PutObjectOnTable(Controller):
         # 4. Disable the avatar's cameras.
         # 5. Set the stickiness of the avatar's left mitten. Set a high drag. Rotate the head.
         # 6. Request AvatarChildrenNames data and Bounds data (this frame only).
+        # 7. Strengthen the left shoulder a bit.
         commands.extend([self.get_add_object("jug05",
                                              position={"x": -0.417, "y": 0.197, "z": 0.139},
                                              rotation={"x": 90, "y": 0, "z": 0},
@@ -75,17 +83,21 @@ class PutObjectOnTable(Controller):
                           "avatar_id": a},
                          {"$type": "rotate_head_by",
                           "axis": "pitch",
-                          "angle": 25},
+                          "angle": 5},
                          {"$type": "send_avatar_children_names",
                           "ids": [a],
                           "frequency": "once"},
                          {"$type": "send_bounds",
                           "ids": [table_id],
-                          "frequency": "once"}])
+                          "frequency": "once"},
+                         {"$type": "adjust_joint_force_by",
+                          "delta": 2,
+                          "joint": "shoulder_left",
+                          "axis": "pitch"}])
         # 7. Add a 3rd-person camera.
         commands.extend(TDWUtils.create_avatar(avatar_type="A_Img_Caps_Kinematic",
                                                avatar_id="c",
-                                               position={"x": 3.9, "y": 2.3, "z": 4.3}))
+                                               position={"x": -3.9, "y": 2.3, "z": 4.3}))
         # 8. Request StickyMittenAvatar and Images data per-frame.
         commands.extend([{"$type": "set_pass_masks",
                           "pass_masks": ["_img"],
@@ -129,13 +141,15 @@ class PutObjectOnTable(Controller):
                         "is_left": True,
                         "avatar_id": a})
 
+        # The initial distance to the table is just the z coordinate (since the x coordinates are the same).
+        d_0 = table_position["z"]
         # Move to the table.
         move_to_table = True
         # The position of the side of the table the avatar is aiming for.
         table_side_position = {"x": table_position["x"], "y": 0, "z": table_position["z"] - table_size[2]}
         while move_to_table:
             # Stop moving if we are close enough.
-            if TDWUtils.get_distance(table_side_position, TDWUtils.array_to_vector3(self.avatar_position)) < 3:
+            if TDWUtils.get_distance(table_side_position, TDWUtils.array_to_vector3(self.avatar_position)) < d_0 * 0.45:
                 move_to_table = False
             # Keep moving forward.
             else:
@@ -147,13 +161,119 @@ class PutObjectOnTable(Controller):
         while not stopped:
             avsm = self._do_frame({"$type": "move_avatar_forward_by",
                                    "avatar_id": a,
-                                   "magnitude": -55})
+                                   "magnitude": -50})
             stopped = np.linalg.norm(avsm.get_velocity()) <= 2
         # Let the avatar coast to a stop.
         stopped = False
         while not stopped:
             avsm = self._do_frame([])
             stopped = np.linalg.norm(avsm.get_velocity()) <= 0.01
+        # Lift up the object.
+        self._bend_arm_joints([{"$type": "rotate_head_by",
+                                "axis": "pitch",
+                                "angle": 20,
+                                "avatar_id": a},
+                               {"$type": "bend_arm_joint_by",
+                                "angle": 25,
+                                "joint": "shoulder_left",
+                                "axis": "pitch",
+                                "avatar_id": a},
+                               {"$type": "bend_arm_joint_by",
+                                "angle": -25,
+                                "joint": "shoulder_left",
+                                "axis": "yaw",
+                                "avatar_id": a},
+                               {"$type": "bend_arm_joint_by",
+                                "angle": 60,
+                                "joint": "shoulder_left",
+                                "axis": "roll",
+                                "avatar_id": a},
+                               {"$type": "bend_arm_joint_by",
+                                "angle": 100,
+                                "joint": "elbow_left",
+                                "axis": "pitch",
+                                "avatar_id": a}])
+        mitten_over_table = False
+        target_z = table_side_position["z"] + 0.15
+        # Keep lifting until the mitten is over the table.
+        while not mitten_over_table:
+            avsm = self._bend_arm_joints([{"$type": "bend_arm_joint_by",
+                                           "angle": 15,
+                                           "joint": "shoulder_left",
+                                           "axis": "pitch",
+                                           "avatar_id": a},
+                                          {"$type": "bend_arm_joint_by",
+                                           "angle": -10,
+                                           "joint": "elbow_left",
+                                           "axis": "pitch",
+                                           "avatar_id": a}])
+            # Get the mitten.
+            for i in range(avsm.get_num_body_parts()):
+                if avsm.get_body_part_id(i) == mitten_left_id:
+                    # Check if the mitten is over the table.
+                    mitten_over_table = avsm.get_body_part_position(i)[2] >= target_z
+
+        # Drop the arm gently.
+        mitten_near_table = False
+        while not mitten_near_table:
+            avsm = self._bend_arm_joints([{"$type": "bend_arm_joint_by",
+                                           "angle": -25,
+                                           "joint": "shoulder_left",
+                                           "axis": "pitch",
+                                           "avatar_id": a}])
+            # Get the mitten.
+            for i in range(avsm.get_num_body_parts()):
+                if avsm.get_body_part_id(i) == mitten_left_id:
+                    # Check if the mitten very close to the table surface.
+                    mitten_near_table = avsm.get_body_part_position(i)[1] - table_size[1] <= 0.1
+        # Drop the object.
+        self._do_frame({"$type": "put_down",
+                        "is_left": True,
+                        "avatar_id": a})
+        # Back away from the table.
+        mitten_away_from_table = False
+        while not mitten_away_from_table:
+            avsm = self._do_frame({"$type": "move_avatar_forward_by",
+                                   "avatar_id": a,
+                                   "magnitude": -50})
+            # Get the mitten.
+            for i in range(avsm.get_num_body_parts()):
+                if avsm.get_body_part_id(i) == mitten_left_id:
+                    # Check if the mitten has cleared the table.
+                    mitten_away_from_table = avsm.get_body_part_position(i)[2] < target_z
+        # Drop the arm.
+        self._do_frame([{"$type": "rotate_head_by",
+                         "axis": "pitch",
+                         "angle": -25},
+                        {"$type": "bend_arm_joint_to",
+                         "angle": 0,
+                         "joint": "shoulder_left",
+                         "axis": "pitch",
+                         "avatar_id": a},
+                        {"$type": "bend_arm_joint_to",
+                         "angle": 0,
+                         "joint": "shoulder_left",
+                         "axis": "yaw",
+                         "avatar_id": a},
+                        {"$type": "bend_arm_joint_to",
+                         "angle": 0,
+                         "joint": "shoulder_left",
+                         "axis": "roll",
+                         "avatar_id": a},
+                        {"$type": "bend_arm_joint_to",
+                         "angle": 0,
+                         "joint": "elbow_left",
+                         "axis": "pitch",
+                         "avatar_id": a}])
+        # Back away from the table.
+        away_from_table = False
+        while not away_from_table:
+            self._do_frame({"$type": "move_avatar_forward_by",
+                            "avatar_id": a,
+                            "magnitude": -20})
+            away_from_table = TDWUtils.get_distance(TDWUtils.array_to_vector3(self.avatar_position), table_position) > 2
+        # Let the joints drop.
+        self._bend_arm_joints([])
 
     def _do_frame(self, commands: Union[List[dict], dict]) -> AvatarStickyMitten:
         """
@@ -194,110 +314,37 @@ class PutObjectOnTable(Controller):
         self.frame_number += 1
         return avsm
 
+    def _bend_arm_joints(self, commands: Union[List[dict], dict]) -> AvatarStickyMitten:
+        """
+        Send commands to bend the arm joints. Wait until the joints stop moving.
 
-"""
-        self.communicate()
+        :param commands: The commands for this frame.
 
-        # Add the avatar and the object.
-        self.communicate()
-        o_id =
+        :return: The avatar data.
+        """
 
-        # 1. Disable the SensorContainer to ensure that the FollowCamera will render to the screen.
-        # 2. Scale the object to be more visible.
-        # 3. Set the back of the left mitten as "sticky".
-        # 4. Set a high angular drag for the avatar to ensure that it won't spin like a top.
-        # 5. Rotate the head to look down at the object.
-        # 6. Pick up the object. Set a high grip to ensure that the avatar won't drop the object.
-        # 7. Bend the arm joints.
-        #
-        # NOTE: We don't specify avatar_id because the default is "a",
-        #       which is what TDWUtils.create_avatar sets it to.
-        self.communicate([
+        avsm = self._do_frame(commands)
 
-
-                          
-                          {"$type": "pick_up_proximity",
-                           "distance": 20,
-                           "grip": 10000,
-                           "is_left": True},
-                          {"$type": "bend_arm_joint_by",
-                           "angle": 25,
-                           "joint": "shoulder_left",
-                           "axis": "pitch"},
-                          {"$type": "bend_arm_joint_by",
-                           "angle": -25,
-                           "joint": "shoulder_left",
-                           "axis": "yaw"},
-                          {"$type": "bend_arm_joint_by",
-                           "angle": 60,
-                           "joint": "shoulder_left",
-                           "axis": "roll"},
-                          {"$type": "bend_arm_joint_by",
-                           "angle": 100,
-                           "joint": "elbow_left",
-                           "axis": "pitch"},
-                          {"$type": "bend_arm_joint_by",
-                           "angle": 15,
-                           "joint": "shoulder_right",
-                           "axis": "pitch"},
-                          {"$type": "bend_arm_joint_by",
-                           "angle": -15,
-                           "joint": "shoulder_right",
-                           "axis": "yaw"},
-                          {"$type": "bend_arm_joint_by",
-                           "angle": 35,
-                           "joint": "elbow_right",
-                           "axis": "pitch"}])
-
-        # Let the simulation run for a while to allow the arm joints to bend.
-        for i in range(100):
-            self.communicate([])
-        # Move the avatar forward.
-        for i in range(20):
-            self.communicate({"$type": "move_avatar_forward_by",
-                              "magnitude": 50})
-        for i in range(100):
-            self.communicate([])
-        # Drop the object and bend the joints down.
-        self.communicate([{"$type": "rotate_head_by",
-                           "axis": "pitch",
-                           "angle": 0},
-                          {"$type": "put_down",
-                           "is_left": True},
-                          {"$type": "bend_arm_joint_to",
-                           "angle": 0,
-                           "joint": "shoulder_left",
-                           "axis": "pitch"},
-                          {"$type": "bend_arm_joint_to",
-                           "angle": 0,
-                           "joint": "shoulder_left",
-                           "axis": "yaw"},
-                          {"$type": "bend_arm_joint_to",
-                           "angle": 0,
-                           "joint": "shoulder_left",
-                           "axis": "roll"},
-                          {"$type": "bend_arm_joint_to",
-                           "angle": 0,
-                           "joint": "elbow_left",
-                           "axis": "pitch"},
-                          {"$type": "bend_arm_joint_to",
-                           "angle": 0,
-                           "joint": "shoulder_right",
-                           "axis": "pitch"},
-                          {"$type": "bend_arm_joint_to",
-                           "angle": 0,
-                           "joint": "shoulder_right",
-                           "axis": "yaw"},
-                          {"$type": "bend_arm_joint_to",
-                           "angle": 0,
-                           "joint": "elbow_right",
-                           "axis": "pitch"}])
-        for i in range(100):
-            self.communicate([])
+        # Get the body part rotations.
+        body_part_rotations: Dict[int, np.array] = dict()
+        for i in range(avsm.get_num_body_parts()):
+            body_part_rotations[avsm.get_body_part_id(i)] = np.array(avsm.get_body_part_rotation(i))
+        # Wait for the joints to stop rotating.
+        done_rotating = False
+        while not done_rotating:
+            avsm = self._do_frame([])
+            bpr: Dict[int, np.array] = dict()
+            done_rotating = True
+            for j in range(avsm.get_num_body_parts()):
+                b_id = avsm.get_body_part_id(j)
+                bpr[b_id] = np.array(avsm.get_body_part_rotation(j))
+                if np.linalg.norm(bpr[b_id] - body_part_rotations[b_id]) > 0.001:
+                    done_rotating = False
+            body_part_rotations.clear()
+            for b_id in bpr:
+                body_part_rotations[b_id] = bpr[b_id]
+        return avsm
 
 
-if __name__ == "__main__":
-    StickyMittenAvatar().run()
-"""
 if __name__ == "__main__":
     PutObjectOnTable(launch_build=False, check_version=False).run()
