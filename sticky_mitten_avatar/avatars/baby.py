ef7c1a37f9b1a420ef3324d223f13d91422e1f96
alters-mit <alters@mit.edu>
2020-08-18 10:40:22 -0400

    close!

--- sticky_mitten_avatar/avatars/baby.py
+++ sticky_mitten_avatar/avatars/baby.py
@@ -6,36 +6,35 @@ from ikpy.link import OriginLink, URDFLink
 class Baby(Avatar):
     def _get_left_arm(self) -> Chain:
         return Chain(name="left_arm", links=[
-            OriginLink(),
             URDFLink(name="shoulder_pitch",
-                     translation_vector=[-0.235, 0.565, 0.075],
+                     translation_vector=[0, 0, 0],
                      orientation=[-1.5708, 0, 0],
-                     rotation=[-1, 0, 0],
+                     rotation=[1, 0, 0],
                      bounds=(-1.0472, 3.12414)),
             URDFLink(name="shoulder_yaw",
-                     translation_vector=[-0.235, 0.565, 0.075],
-                     orientation=[-1.5708, 0, 0],
-                     rotation=[0, 1, 0],
+                     translation_vector=[0, 0, 0],
+                     orientation=[0, 0, 0],
+                     rotation=[0, -1, 0],
                      bounds=(-1.5708, 1.5708)),
             URDFLink(name="shoulder_roll",
-                     translation_vector=[-0.235, 0.565, 0.075],
-                     orientation=[-1.5708, 0, 0],
-                     rotation=[0, 0, 1],
+                     translation_vector=[0, 0, 0],
+                     orientation=[0, 0, 0],
+                     rotation=[0, 0, -1],
                      bounds=(-0.785398, 0.785398)),
             URDFLink(name="elbow_pitch",
-                     translation_vector=[-0.235, 0.329993, 0.075],
-                     orientation=[-1.5708, 0, 0],
-                     rotation=[-1, 0, 0],
+                     translation_vector=[0, -0.329993, 0],
+                     orientation=[0, 0, 0],
+                     rotation=[1, 0, 0],
                      bounds=(0, 2.79253)),
             URDFLink(name="wrist_roll",
-                     translation_vector=[-0.235, 0.179993, 0.075],
-                     orientation=[-1.5708, 0, 0],
-                     rotation=[0, 0, 1],
+                     translation_vector=[0, -0.179993, 0],
+                     orientation=[0, 0, 0],
+                     rotation=[0, 0, -1],
                      bounds=(-1.5708, 1.5708)),
             URDFLink(name="wrist_pitch",
-                     translation_vector=[-0.235, 0.179993, 0.075],
-                     orientation=[-1.5708, 0, 0],
-                     rotation=[-1, 0, 0],
+                     translation_vector=[0, 0, 0],
+                     orientation=[0, 0, 0],
+                     rotation=[1, 0, 0],
                      bounds=(0, 1.5708))])
 
     def _get_right_arm(self) -> Chain:
@@ -71,3 +70,46 @@ class Baby(Avatar):
                      orientation=[-1.5708, 0, 0],
                      rotation=[-1, 0, 0],
                      bounds=(0, 1.5708))])
+
+
+if __name__ == "__main__":
+    q = Chain(name="left_arm", links=[
+            URDFLink(name="shoulder_pitch",
+                     translation_vector=[0, 0, 0],
+                     orientation=[-1.5708, 0, 0],
+                     rotation=[1, 0, 0],
+                     bounds=(-1.0472, 3.12414)),
+            URDFLink(name="shoulder_yaw",
+                     translation_vector=[0, 0, 0],
+                     orientation=[0, 0, 0],
+                     rotation=[0, -1, 0],
+                     bounds=(-1.5708, 1.5708)),
+            URDFLink(name="shoulder_roll",
+                     translation_vector=[0, 0, 0],
+                     orientation=[0, 0, 0],
+                     rotation=[0, 0, -1],
+                     bounds=(-0.785398, 0.785398)),
+            URDFLink(name="elbow_pitch",
+                     translation_vector=[0, -0.329993, 0],
+                     orientation=[0, 0, 0],
+                     rotation=[1, 0, 0],
+                     bounds=(0, 2.79253)),
+            URDFLink(name="wrist_roll",
+                     translation_vector=[0, -0.179993, 0],
+                     orientation=[0, 0, 0],
+                     rotation=[0, 0, 1],
+                     bounds=(-1.5708, 1.5708)),
+            URDFLink(name="wrist_pitch",
+                     translation_vector=[0, 0, 0],
+                     orientation=[0, 0, 0],
+                     rotation=[-1, 0, 0],
+                     bounds=(0, 1.5708))
+    ])
+    import matplotlib.pyplot
+    import ikpy.utils.plot as plot_utils
+    fig, ax = plot_utils.init_3d_figure()
+    target = [-0.4, 0.4, 0.271]
+    rots = q.inverse_kinematics(target_position=target)
+
+    q.plot(rots, ax, target=target)
+    matplotlib.pyplot.show()
