d6a403a75fef053f60ed93f8fe85ca7565e35386
alters <alters@mit.edu>
2020-08-27 14:43:21 -0400

    pick_up_test

--- tests/ik_unit_tests.py
+++ tests/ik_unit_tests.py
@@ -67,9 +67,31 @@ class IKUnitTests(StickyMittenAvatarController):
                           "avatar_id": self.id})
         self.bend_arm(avatar_id=self.id, target={"x": -0.2, "y": 0.4, "z": 0.385}, arm=Arm.left)
 
+    def pick_up_test(self) -> None:
+        """
+        Test: The avatar picks up the object. The avatar is at a non-origin position and rotation.
+        """
+
+        o_id = self.get_unique_id()
+        self.communicate([{"$type": "teleport_avatar_to",
+                           "position": {"x": 1.1, "y": 0.0, "z": 1},
+                           "avatar_id": self.id},
+                          {"$type": "rotate_avatar_by",
+                           "angle": -15,
+                           "axis": "yaw",
+                           "is_world": True,
+                           "avatar_id": self.id}])
+        self.communicate(self.get_add_object("jug05",
+                                             position={"x": 0.9, "y": 0, "z": 1.385},
+                                             object_id=o_id,
+                                             scale={"x": 0.8, "y": 0.8, "z": 0.8}))
+        self.pick_up(avatar_id=self.id, object_id=o_id)
+        assert o_id in self._avatars[self.id].frame.get_held_left()
+
 
 if __name__ == "__main__":
     c = IKUnitTests()
+    c.do_test(c.pick_up_test)
     c.do_test(c.symmetry)
     c.do_test(c.rotation)
     c.do_test(c.position)
