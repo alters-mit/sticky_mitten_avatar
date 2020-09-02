ed1e496814e259422d3353e736ce652227d650bc
alters <alters@mit.edu>
2020-09-02 13:11:52 -0400

    updated shake demo

--- controllers/shake_demo.py
+++ controllers/shake_demo.py
@@ -18,20 +18,20 @@ if __name__ == "__main__":
     for container, arm, x_dir in zip([c.container_0, c.container_1], [Arm.right, Arm.left], [1, -1]):
         c.go_to(avatar_id=c.avatar_id, target=container, move_stopping_threshold=0.7)
         c.pick_up(avatar_id=c.avatar_id, object_id=container)
-        c.bend_arm(avatar_id=c.avatar_id, target={"x": 0.2 * x_dir, "y": 0.4, "z": 0.385}, arm=arm)
+        c.bend_arm(avatar_id=c.avatar_id, target={"x": 0.1 * x_dir, "y": 0.4, "z": 0.485}, arm=arm)
         c.shake(avatar_id=c.avatar_id, joint_name=f"elbow_{arm.name}")
         c.put_down(avatar_id=c.avatar_id, do_motion=False)
     # Pick up the first container again.
     c.go_to(avatar_id=c.avatar_id, target=c.container_0, move_stopping_threshold=0.7)
+    c.turn_to(avatar_id=c.avatar_id, target=c.container_0)
     c.pick_up(avatar_id=c.avatar_id, object_id=c.container_0)
 
     # Put the container on the sofa.
     # The "target" values are hardcoded for now; in the future, there will be better avatar navigation.
-    c.bend_arm(avatar_id=c.avatar_id, target={"x": 0.026, "y": 0.2, "z": 0.66}, arm=Arm.right)
     c.go_to(avatar_id=c.avatar_id,
             target={"x": 1.721, "y": 0, "z": -1.847},
             turn_stopping_threshold=2,
-            move_stopping_threshold=0.1)
+            move_stopping_threshold=0.2)
     c.turn_to(avatar_id=c.avatar_id, target={"x": 2.024, "y": 0.46, "z": -2.399})
     c.put_down(avatar_id=c.avatar_id)
     c.end()
