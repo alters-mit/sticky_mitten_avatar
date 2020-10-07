f9ba9400db2fe4e4156c114340288b3066f5b48f
alters <alters@mit.edu>
2020-10-07 14:39:54 -0400

    Test works without overlap sphere

--- tests/in_box_test.py
+++ tests/in_box_test.py
@@ -1,6 +1,7 @@
+from typing import Dict, List
 from tdw.controller import Controller
 from tdw.tdw_utils import TDWUtils, QuaternionUtils
-from tdw.output_data import OutputData, Collision, EnvironmentCollision, Rigidbodies, OverlapSphere
+from tdw.output_data import OutputData, Collision, EnvironmentCollision, Rigidbodies
 from sticky_mitten_avatar.util import get_data, get_collisions
 
 
@@ -11,14 +12,23 @@ if __name__ == "__main__":
     c = Controller(launch_build=False)
     c.start()
     resp = c.communicate([TDWUtils.create_empty_room(12, 12),
-                          c.get_add_object("basket_18inx18inx12iin", object_id=basket_id),
-                          c.get_add_object("pepper", object_id=o_id, position={"x": 0, "y": 0.08, "z": 0}),
-                          c.get_add_object("pepper", object_id=p_id, position={"x": 0, "y": 0.22, "z": 0.24}),
+                          c.get_add_object("basket_18inx18inx12iin",
+                                           object_id=basket_id),
+                          c.get_add_object("pepper", object_id=o_id,
+                                           position={"x": 0, "y": 0.08, "z": 0},
+                                           rotation={"x": 60, "y": -70, "z": 5}),
+                          c.get_add_object("pepper", object_id=p_id,
+                                           position={"x": 0, "y": 0.22, "z": 0.24}),
                           {"$type": "set_sleep_threshold",
                            "sleep_threshold": 0.1},
                           {"$type": "send_rigidbodies",
-                           "frequency": "always"}])
+                           "frequency": "always"},
+                          {"$type": "send_collisions",
+                           "enter": True,
+                           "stay": True,
+                           "exit": True}])
     sleeping = False
+    stays: List[Collision] = list()
     while not sleeping:
         resp = c.communicate([])
         rigidbodies = get_data(resp=resp, d_type=Rigidbodies)
@@ -27,13 +37,21 @@ if __name__ == "__main__":
         for i in range(rigidbodies.get_num()):
             if rigidbodies.get_id(i) == o_id:
                 o_sleep = rigidbodies.get_sleeping(i)
+                collisions, env_collisions = get_collisions(resp=resp)
+                if not o_sleep:
+                    stays.clear()
+                    collisions, env_collisions = get_collisions(resp=resp)
+                    for coll in collisions:
+                        if coll.get_state() == "stay" and (coll.get_collider_id() == o_id or coll.get_collidee_id() == o_id):
+                            stays.append(coll)
             elif rigidbodies.get_id(i) == p_id:
                 p_sleep = rigidbodies.get_sleeping(i)
         sleeping = o_sleep and p_sleep
-    resp = c.communicate({"$type": "send_overlap_sphere",
-                          "position": {"x": 0, "y": 0, "z": 0},
-                          "radius": 0.19})
-    sphere = get_data(resp=resp, d_type=OverlapSphere)
-    sphere_ids = sphere.get_object_ids()
-    assert o_id in sphere_ids
-    assert p_id not in sphere_ids
+
+    assert len(stays) > 0
+    coll = stays[0]
+    assert coll.get_state() == "stay"
+    assert coll.get_collidee_id() == o_id or coll.get_collider_id() == o_id
+    assert coll.get_collidee_id() == basket_id or coll.get_collider_id() == basket_id
+    for i in range(coll.get_num_contacts()):
+        assert coll.get_contact_normal(i)[1] > 0
