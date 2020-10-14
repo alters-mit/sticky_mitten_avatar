4e59cab6907ca469677cc913714226f1825a556d
alters <alters@mit.edu>
2020-10-14 13:54:24 -0400

    Improved controller

--- controllers/put_in_held_container.py
+++ controllers/put_in_held_container.py
@@ -4,7 +4,8 @@ from sticky_mitten_avatar import StickyMittenAvatarController, Arm
 
 class PutInHeldContainer(StickyMittenAvatarController):
     def __init__(self, port: int = 1071):
-        super().__init__(port=port, launch_build=False, id_pass=False)
+        # Set demo=False for an actual simulation.
+        super().__init__(port=port, launch_build=False, id_pass=False, demo=True)
         self.container_id = 0
         self.object_id = 1
 
@@ -26,6 +27,9 @@ if __name__ == "__main__":
     c = PutInHeldContainer()
     c.init_scene()
 
+    # This will make the simulation run slower and should only be added for demoing or debugging.
+    c.add_overhead_camera({"x": -0.08, "y": 1.25, "z": 1.41}, target_object="a", images="cam")
+
     # Grasp each object.
     c.grasp_object(object_id=c.object_id, arm=Arm.right)
     c.grasp_object(object_id=c.container_id, arm=Arm.left)
@@ -33,3 +37,4 @@ if __name__ == "__main__":
     # Put the object in the container.
     c.put_in_container(object_id=c.object_id, container_id=c.container_id, arm=Arm.right)
     c.reset_arm(arm=Arm.right)
+    c.end()
